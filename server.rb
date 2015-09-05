f4c304ef09be7cd4226d61556c86006ad66ad708
Jacob <jakegibs617@gmail.com>
2015-09-04 20:40:42 -0400

    added gallery, slides, form box

--- server.rb
+++ server.rb
@@ -1,11 +1,18 @@
 require 'sinatra'
 
 get '/' do
-  redirect '/shows/about'
+  redirect '/shows/home'
 end
 
-get '/shows/paintings/2015' do
-  erb :"/shows/paintings/2015"
+get '/shows/home' do
+  erb :"/shows/home"
+end
+get '/shows/paintings/gallery' do
+  erb :"/shows/paintings/gallery"
+end
+
+get '/shows/paintings/2015/slides' do
+  erb :"/shows/paintings/2015/slides"
 end
 
 get '/shows/drawings/2015' do
@@ -14,6 +21,9 @@ end
 
 get '/shows/about' do
   erb :"/shows/about"
+end
+get '/shows/links' do
+  erb :"/shows/links"
 end
 
 get '/shows/contact' do
